9b8c04f808b1faa30f463dc5888ffe3a038a5630
cvemis9itu <cvemis9itu@gmail.com>
2026-03-28 20:38:48 +0800

    fix py

--- reports/config/run.py
+++ reports/config/run.py
@@ -2,12 +2,12 @@ import yaml
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 with open("query.yaml", "r", encoding="utf-8") as f:
-        config = yaml.safe_load(f)
-print('数据库:',config['database'])
-print('时间范围:',config['time_window']['start'],'-',config['time_window']['end'])
-print("检索关键词：")
-for kw in config["keywords"]:
-        print(kw)
-print("分析方法：")
-for am in config["analysis_methods"]:
-    print(am)
+        config1 = yaml.safe_load(f)
+with open("query.yaml", "r", encoding="utf-8") as g:
+        config2 = yaml.safe_load(g)
+print('object',config1['query']['object'])
+print('time',config1['query']['constraint']['time']['start'],'-',config1['query']['constraint']['time']['end'])
+print('method',config1['query']['method'])
+print('context',config1['query']['context'])
+print('document_type',config1['query']['constraint']["document_type"])
+
